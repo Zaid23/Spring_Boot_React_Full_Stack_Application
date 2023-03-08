e5c586f973fac97b0fabeb9837182ecf5d61f585
Zaid23 <zaidshaikhofficial@gmail.com>
2023-03-08 12:04:01 +0530

    Backend Added

---
+++ src/main/java/com/codewitharjun/fullstackbackend/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.codewitharjun.fullstackbackend.controller;
+
+import com.codewitharjun.fullstackbackend.exception.UserNotFoundException;
+import com.codewitharjun.fullstackbackend.model.User;
+import com.codewitharjun.fullstackbackend.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/adduser")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id).map( user -> {
+            user.setName(newUser.getName());
+            user.setUsername(newUser.getUsername());
+            user.setEmail(newUser.getEmail());
+            return userRepository.save(user);
+        }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if(!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id " + id + " has been deleted successfully";
+    }
+}
